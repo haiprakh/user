f8ffe1491236a9a6b03554935d25667ec99cf8c3
Haiprakhonpokklaw <50812972+Haiprakhonpokklaw@users.noreply.github.com>
2019-05-20 08:16:23 +0700

    setting.php

---
+++ setting.php
@@ -0,0 +1,22 @@
+<?php
+/* setting bot */
+$ads=$ijo."    [ official chanel adi bordir ]".$t.$red."\t >> subcribe chanel << ".$t.$pth."[ ".$ijo."AW 2000".$pth." ]=[".$kn." Adi Bordir ".$pth."]=[ ".$tr."HĂN C_GAMER ]".$t;
+
+$donasi=false;
+
+$warn=$red."[!] warning !!!
+".$kn."[-] ini adalah program ilegal
+".$kn."[-] resiko sepenuhnya di tanggung pengguna".$t;
+
+$key="adidoank";
+$link_yogo="https://youtu.be/hLkAqZM3QHc";
+
+
+$epi=$kn."
+──▄▀──▄▀▄──▄▀▀──█────█────▀▄─▄▀──
+──█───█▀█───▀▄──█▀▄──█─▄────█────
+───▀──▀─▀──▀▀───▀─▀──▀▀▀────▀────
+ ".$pth."[ ".$tr."v 1.0".$pth." ]==[ ".$tr."creator:".$ijo." adidoank".$pth." ]==[".$tr." ref: ".$ijo."TVFG9W".$pth." ]".$t;
+$msg_epi=$ijo."[•] update skrip https://github.com/adidoank/epicash".$t.$pth."[support:".$tr." khairil".$pth."][Kode: ".$ijo."ZAVGFX]".$t;
+$stat_epi1=true;
+
